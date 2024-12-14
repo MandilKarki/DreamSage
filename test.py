2e4d147bd0dcbecb37c1728868764d4c5f3b34ce
MandilKarki <71919875+MandilKarki@users.noreply.github.com>
2024-12-13 17:09:42 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
+https://www.promptingguide.ai/techniques/tot
+
+microsoft/Phi-3.5-mini-instruct
+
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-1.5B-Instruct")
+model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-1.5B-Instruct")
+
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-llm-7b-base")
+model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-llm-7b-base")
+
+
+improve exisiting tprompt anthropic console https://console.anthropic.com/dashboard
 
